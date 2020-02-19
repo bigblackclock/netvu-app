57af40c0f7b05998ebc8be23dd9822e4f4f8c7dd
bigblackclock <abdlhadi1999apdo@gmail.com>
2020-02-19 21:32:55 +0100

    make some design changes

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:netvu/models/device.dart';
+import 'package:neumorphic/neumorphic.dart';
+import 'package:neuomorphic_container/neuomorphic_container.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -63,6 +65,8 @@ class _HomePageState extends State<HomePage> {
   Widget BodyWidget(LastPing lastPing) {
     return Column(
       children: <Widget>[
+        SizedBox(height: 10,),
+       
         Container(
           color: Colors.yellow,
           child: Text(
@@ -70,26 +74,44 @@ class _HomePageState extends State<HomePage> {
             style: TextStyle(fontSize: 16),
           ),
         ),
+        SizedBox(height: 10,),
+
         Dashbored(lastPing),
+        SizedBox(height: 10,),
         Flexible(
-          child: ListView.builder(
-              itemCount: lastPing.devices.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  subtitle: (Text(lastPing.devices[index].vendor)),
-                  title: Text(lastPing.devices[index].ip),
-                  trailing: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: lastPing.devices[index].status == true
-                            ? Colors.green
-                            : Colors.red),
+                  child: NeuomorphicContainer(
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                        color: Color.fromRGBO(209, 205, 199, 1.0), width: 2.0),
+                        ),
+                child: ListView.builder(
+                    itemCount: lastPing.devices.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        subtitle: (Text(lastPing.devices[index].vendor)),
+                        title: Text(lastPing.devices[index].ip),
+                        trailing: Container(
+                          width: 15,
+                          height: 15,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: lastPing.devices[index].status == true
+                                  ? Colors.green
+                                  : Colors.red),
+                        ),
+                      );
+                    }),
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              ),
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                  color: Color.fromRGBO(239, 238, 238, 1.0), width: 3.0),
                   ),
-                );
-              }),
+        
         ),
+        SizedBox(height: 20,)
       ],
     );
   }
@@ -133,88 +155,83 @@ class _HomePageState extends State<HomePage> {
       }
     }
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget>[
-        new Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 2.0, color: Colors.blue),
-            borderRadius: BorderRadius.all(
-                Radius.circular(5.0) //         <--- border radius here
+        NeuomorphicContainer(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 60,
+                width: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('All Devices',
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold)),
+                    Text(
+                      devices.length.toString(),
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
                 ),
-          ),
-          child: new Container(
-            height: 60,
-            width: 100,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('All Devices',
-                    style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold)),
-                Text(
-                  devices.length.toString(),
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
+              ),
             ),
-          ),
-        ),
-        new Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 2.0, color: Colors.green),
-            borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          ),
-          child: new Container(
-            height: 60,
-            width: 100,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('Online Devices',
-                    style: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.bold)),
-                Text(
-                  listOn.length.toString(),
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
+            borderRadius: BorderRadius.circular(15.0)),
+        NeuomorphicContainer(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 60,
+                width: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('Online',
+                        style: TextStyle(
+                            color: Colors.green, fontWeight: FontWeight.bold)),
+                    Text(
+                      listOn.length.toString(),
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-        ),
-        new Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 2.0, color: Colors.red),
-            borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          ),
-          child: new Container(
-            height: 60,
-            width: 100,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('Offline Devices',
-                    style: TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.bold)),
-                Text(
-                  listOff.length.toString(),
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
+            borderRadius: BorderRadius.circular(15.0)),
+        NeuomorphicContainer(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 60,
+                width: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('Offline',
+                        style: TextStyle(
+                            color: Colors.red, fontWeight: FontWeight.bold)),
+                    Text(
+                      listOff.length.toString(),
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-        ),
+            borderRadius: BorderRadius.circular(15.0)),
       ],
     );
   }
